1061a111a4dbdc874222ac657dea10c3fc1821e7
Lucas Payton <lucasjpayton101@gmail.com>
2021-11-16 12:14:35 -0700

    Howdy

--- PlanIt.client/src/components/Thread.vue
+++ PlanIt.client/src/components/Thread.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="row">
     <div class="col-12">
-    <h1>This is where the thread will be</h1>
+      <h1>Hello</h1>
     </div>
   </div>
 </template>
 <script>
 export default {
   setup() {
-    
+
   },
 }
 </script>
 <style scoped>
-
 </style>
