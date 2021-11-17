d56335210d69d88edcff07853edeb405504d4a74
Lucas Payton <lucasjpayton101@gmail.com>
2021-11-17 14:10:29 -0700

    Project Page Base

---
+++ PlanIt.client/src/components/Sprints.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <div class="card p-3 elevation-2">
+        <div class="card-text">Hello</div>
+      </div>
+    </div>
+  </div>
+</template>
--- PlanIt.client/src/pages/ProjectPage.vue
+++ PlanIt.client/src/pages/ProjectPage.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="project container">
+  <div class="project container-fluid">
     <div class="row">
       <div class="col-1"></div>
-      <div class="col-10">
-        <Thread />
+      <div class="col-10 flex-column">
+        <ProjectInfo />
       </div>
       <div class="col-1"></div>
     </div>
@@ -15,11 +15,15 @@
 import { onMounted } from "@vue/runtime-core"
 import { projectsService } from "../services/ProjectsService"
 import { useRoute } from "vue-router"
+import { logger } from "../utils/Logger"
+import { AppState } from "../AppState"
 export default {
   setup() {
     const route = useRoute();
     onMounted(async () => {
+      logger.log(route.params.id)
       projectsService.setActive(route.params.id)
+      logger.log(AppState.activeProject)
     })
     return {}
   }
